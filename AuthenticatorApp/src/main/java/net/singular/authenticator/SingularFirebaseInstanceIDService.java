b6b1daf1a9c5e98862d0c88d6ecb8ca6c91a5c20
Ruby Feinstein <ruby@singular.net>
2016-06-15 00:20:51 +0300

    added super basic FCM message handler

--- AuthenticatorApp/src/main/java/net/singular/authenticator/SingularFirebaseInstanceIDService.java
+++ AuthenticatorApp/src/main/java/net/singular/authenticator/SingularFirebaseInstanceIDService.java
@@ -1,20 +1,20 @@
 package net.singular.authenticator;
 
 import android.content.Intent;
+import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
 public class SingularFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    private static final String TAG = "SingularFIIDService";
     private String mRegistrationID = null;
 
     @Override
     public void onTokenRefresh() {
         mRegistrationID = FirebaseInstanceId.getInstance().getToken();
 
-        Intent i = new Intent(getApplicationContext(), AuthenticatorActivity.class);
-        i.putExtra("RegistrationID", mRegistrationID);
+        Log.d(TAG, "Refreshed token: " + mRegistrationID);
 
-        startActivity(i);
     }
 }
--- AuthenticatorApp/src/main/java/net/singular/authenticator/SingularFirebaseMessagingService.java
+++ AuthenticatorApp/src/main/java/net/singular/authenticator/SingularFirebaseMessagingService.java
@@ -12,20 +12,57 @@ import android.util.Log;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
+
 public class SingularFirebaseMessagingService extends FirebaseMessagingService {
 
-    private static final String TAG = "SingularFirebaseMessagingService";
+    private static final String TAG = "SingularFMS";
 
     /**
      * Called when message is received.
      *
      * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
      */
-
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        String from = remoteMessage.getFrom();
-        String body = remoteMessage.getNotification().getBody();
+        // TODO(developer): Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Map<String, String> msg_data = remoteMessage.getData();
+        String src = msg_data.get("src");
+        // todo: verify that src matches the paired src
+        try {
+            JSONObject value = new JSONObject(msg_data.get("value"));
+            String command = value.getString("command");
+            Log.d(TAG, "command: " + command);
+            switch(command){
+                case "getAccounts":
+                    handleGetAccount();
+                    break;
+                case "getCode":
+                    int id = value.getInt("id");
+                    handleGetCode(id);
+                    break;
+                default:
+                    Log.e(TAG, "unknown command: " + command);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    private void handleGetCode(int id) {
+        Log.d(TAG, "handleGetCode: " + Integer.toString(id));
+    }
+
+    private void handleGetAccount() {
+        Log.d(TAG, "handleGetAccount");
     }
     // [END receive_message]
 
@@ -35,14 +72,13 @@ public class SingularFirebaseMessagingService extends FirebaseMessagingService {
      * @param messageBody FCM message body received.
      */
     private void sendNotification(String messageBody) {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, AuthenticatorActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_stat_ic_notification)
                 .setContentTitle("FCM Message")
                 .setContentText(messageBody)
                 .setAutoCancel(true)
