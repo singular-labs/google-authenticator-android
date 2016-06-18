fa1587393ab4bcc697f496ef302eb2a3690b8832
Ruby Feinstein <ruby@singular.net>
2016-06-18 22:31:28 +0300

    replaced AsyncHttpClient with OkHttp

--- AuthenticatorApp/src/main/java/net/singular/authenticator/SingularFCMProxyProtocol.java
+++ AuthenticatorApp/src/main/java/net/singular/authenticator/SingularFCMProxyProtocol.java
@@ -1,30 +1,34 @@
 package net.singular.authenticator;
 
-import android.os.Looper;
 import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import org.json.*;
-import com.loopj.android.http.*;
 
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
 
-import cz.msebera.android.httpclient.Header;
-import cz.msebera.android.httpclient.entity.StringEntity;
+import okhttp3.Call;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.Callback;
 
 public class SingularFCMProxyProtocol {
-    private AsyncHttpClient client = new AsyncHttpClient();
-    private static final String FCMProxyEndpoint = "http://192.168.1.6:8080/send";
+    private OkHttpClient client = new OkHttpClient();
+    private static final String FCMProxyEndpoint = "https://twofactor.singular.net/two_factor_router";
 
     private String mLocalRegistrationID;
     private String mRemoteRegistrationID;
 
     private static final String TAG = "SingularFCMProxy";
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
 
     SingularFCMProxyProtocol(String localRegistrationID, String remoteRegistrationID)
     {
@@ -39,19 +43,10 @@ public class SingularFCMProxyProtocol {
 
             params.put("command", "Hello");
 
-            this.postJSON(params, new JsonHttpResponseHandler() {
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                    Log.d(TAG, response.toString());
-                }
-            });
+            this.postJSON(params);
         } catch (JSONException e)
         {
             Log.e(TAG, "JSONException");
-        }
-        catch (UnsupportedEncodingException e)
-        {
-            Log.e(TAG, "UnsupportedEncodingException");
         }
     }
 
@@ -63,19 +58,10 @@ public class SingularFCMProxyProtocol {
             params.put("command", "accountList");
             params.put("accounts", new JSONArray(accountList));
 
-            this.postJSON(params, new JsonHttpResponseHandler() {
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                    Log.d(TAG, response.toString());
-                }
-            });
+            this.postJSON(params);
         } catch (JSONException e)
         {
             Log.e(TAG, "JSONException");
-        }
-        catch (UnsupportedEncodingException e)
-        {
-            Log.e(TAG, "UnsupportedEncodingException");
         }
     }
 
@@ -88,19 +74,10 @@ public class SingularFCMProxyProtocol {
             params.put("code", value);
             params.put("id", id);
 
-            this.postJSON(params, new JsonHttpResponseHandler() {
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                    Log.d(TAG, response.toString());
-                }
-            });
+            this.postJSON(params);
         } catch (JSONException e)
         {
             Log.e(TAG, "JSONException");
-        }
-        catch (UnsupportedEncodingException e)
-        {
-            Log.e(TAG, "UnsupportedEncodingException");
         }
     }
 
@@ -112,32 +89,38 @@ public class SingularFCMProxyProtocol {
             params.put("command", "reject");
             params.put("id", id);
 
-            this.postJSON(params, new JsonHttpResponseHandler() {
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                    Log.d(TAG, response.toString());
-                }
-            });
+            this.postJSON(params);
         } catch (JSONException e)
         {
             Log.e(TAG, "JSONException");
         }
-        catch (UnsupportedEncodingException e)
-        {
-            Log.e(TAG, "UnsupportedEncodingException");
-        }
     }
 
-    public void postJSON(JSONObject params, JsonHttpResponseHandler handler) throws JSONException, UnsupportedEncodingException
-    {
+    public void postJSON(JSONObject params) throws JSONException {
         JSONObject proxy_packet = new JSONObject();
 
         proxy_packet.put("to", this.mRemoteRegistrationID);
         proxy_packet.put("from", this.mLocalRegistrationID);
         proxy_packet.put("message", params.toString());
 
-        StringEntity se = new StringEntity(proxy_packet.toString());
 
-        this.client.post(null, SingularFCMProxyProtocol.FCMProxyEndpoint, se, "application/json", handler);
+        RequestBody body = RequestBody.create(JSON, proxy_packet.toString());
+        Request request = new Request.Builder()
+                .url(FCMProxyEndpoint)
+                .post(body)
+                .build();
+
+        Callback callback = new Callback() {
+
+            @Override public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override public void onResponse(Call call, Response response) {
+                Log.d(TAG, response.toString());
+            }
+        };
+
+        client.newCall(request).enqueue(callback);
     }
 }
